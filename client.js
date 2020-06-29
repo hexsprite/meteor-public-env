6dfcf1187abde834bf830bccdc792e1a7da7766e
Jordan Brant Baker <jbb@scryent.com>
2020-06-29 11:00:24 -0700

    fix: handle cases where data is unset, eg. cordova offline

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 
 // will execute before any application code, but before if you have a package that depends on a value from public-env
 Meteor.startup(() => {
-  for (const [key, value] of window.__meteor_public_env) {
+  for (const [key, value] of window.__meteor_public_env || []) {
     Meteor.settings.public[key] = value
   }
 })
